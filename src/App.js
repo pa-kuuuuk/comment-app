41b3a8b44fc6f7d5698f67a509c0094b09de8c72
pa-kuuuuk <parkjungyong0522@gmail.com>
2021-06-01 09:50:07 +0900

    시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(Props){
+    super(Props);
+    this.state ={
+
+    }
+  }
+    render() {
+        return (
+        <div>
+            <h1>회원관리</h1>
+            <div id ="memberList">
+              <table>
+               <tr>
+                 <th>이름</th><th>나이</th><th>전화번호</th><th>주소</th>
+               </tr>
+               <tr>
+                 <td>홍길동</td><td>26</td><td>010-1234-5678</td><td>경기도 성남시</td>
+               </tr>
+              </table>
+            </div>
+            <div>
+              <table id="insertFrom">
+              <tr>
+              <td>이름 :</td> <input></input>
+              </tr><tr>
+              <td>나이 :</td> <input></input>
+              </tr><tr>
+              <td>전화번호 :</td> <input></input>
+              </tr><tr>
+              <td>주소 :</td> <input></input>
+              </tr>
+              </table>
+            </div>
+        </div>
+
+
+        );
+    }
 }
 
 export default App;
